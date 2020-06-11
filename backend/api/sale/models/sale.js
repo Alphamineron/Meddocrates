dd8ebf4972d35c5208a65edd6fde18e417183758
Alphamineron <alphamineron@gmail.com>
2020-06-11 13:44:24 +0530

    Added Integrity Checks on Sales POST route

--- backend/api/sale/models/sale.js
+++ backend/api/sale/models/sale.js
@@ -12,14 +12,46 @@ module.exports = {
             let qSold = result["QuantitySold"];
             let price = result["MedUID"]["Price"];
             
-            strapi.query('sale').update(
+            strapi.query('sale').update(    // This updates the revenue attribute in the sales
                 { id: result["id"] },
                 {
                     Revenue: qSold*price
                 }
             );
 
-            // console.log(strapi.query('medicine').find({ id: result["MedUID"]["id"] })["Price"]);
+
+            // Below code is for maintaining the inventory/racks upon a sale or discarding a sale if it exceeds space
+            var promises = [];
+            promises.push(
+                strapi.services.inventory.find()
+                    .then((data) => {
+                        data = data.filter(function (entry) {
+                                return entry.MedUID.id === result["MedUID"]["id"];
+                        });
+                        return {
+                            "id": data[0].id,
+                            "MedUID": data[0].MedUID.id,
+                            "Quantity": data[0].Quantity - qSold,
+                        };
+                    })
+                    .then((sampleObj) => {
+                        if (sampleObj.Quantity >= 0) {
+                            strapi.query('inventory').update(
+                                { id: sampleObj["id"] },
+                                {
+                                    Quantity: sampleObj["Quantity"]
+                                }
+                            );
+                        }
+                        else {
+                            strapi.query('sale').delete({ id: result["id"] });
+                        }
+                    }).catch ((error) => {
+                        console.log('Error: ', error);
+                    })
+            );
+
+            return Promise.all(promises);
         },
     },
 };
