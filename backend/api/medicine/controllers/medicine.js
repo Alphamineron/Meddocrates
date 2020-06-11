5a1c386049e808e5b6fd3b5b92450edac440f2b2
Alphamineron <alphamineron@gmail.com>
2020-06-11 05:32:26 +0530

    Added Expired Meds Fetch Route

--- backend/api/medicine/controllers/medicine.js
+++ backend/api/medicine/controllers/medicine.js
@@ -5,4 +5,19 @@
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+    async expired(ctx) {
+        const medicines = await strapi.services.medicine.find(ctx.query);
+        // return medicines.map((medicine) => medicine.Name);
+
+        let today = new Date();
+        let currDateStr = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+
+        return medicines.filter(function (entry) {
+            let currDate = new Date(currDateStr);
+            let ExpiryDate = new Date(entry.ExpiryDate);
+
+            return ExpiryDate < currDate;
+        });
+    }
+};
