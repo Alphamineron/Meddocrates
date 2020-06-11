0385b90d2dc9360c34b040940f35072e512abfad
Alphamineron <alphamineron@gmail.com>
2020-06-11 06:59:20 +0530

    Added Sales of Month Fetch Route

--- backend/api/sale/controllers/sale.js
+++ backend/api/sale/controllers/sale.js
@@ -9,7 +9,7 @@ module.exports = {
     async roed(ctx) {
         const sales = await strapi.services.sale.find(ctx.query);
 
-        
+
         let filteredSales = sales.filter(function (entry) {
                                 let givenDate = new Date(ctx.params["date"]);
                                 let SaleDate = new Date(entry.SaleDate);
@@ -28,5 +28,51 @@ module.exports = {
             });
             return filteredSales.map((fsale) => fsale.Revenue).reduce((a, b) => a + b, 0);
         }
+    },
+
+
+
+
+    async tsalesdom(ctx) {
+        const sales = await strapi.services.sale.find(ctx.query);
+        
+        let filteredSales = sales.filter(function (entry) {
+                                // const dateTime = ctx.params["date"];
+                                // const parts = dateTime.split(/[-]/);
+                                
+                                
+                                var currDate = new Date();
+                                let SaleDate = new Date(entry.SaleDate);
+                                var currMonth = currDate.getMonth() + 1;
+                                var currYear = currDate.getFullYear();
+                                var month = SaleDate.getMonth() + 1;
+                                var year = SaleDate.getFullYear();
+
+                                return currMonth == month && year == currYear;
+                            });
+
+        let currMonthSales = {}
+
+        if (Number(ctx.params["medUID"]) === -1) {
+                filteredSales.forEach(record => { currMonthSales[record.SaleDate] = 0; });  // For init of currMonthSales
+                filteredSales.forEach(record => {
+                    console.log(record.SaleDate);
+                    console.log(record.QuantitySold);
+                    currMonthSales[record.SaleDate] += record.QuantitySold || 0;
+                });
+        }
+        else {
+            filteredSales = filteredSales.filter(function (entry) {
+                return entry.MedUID.id === Number(ctx.params["medUID"])
+            });
+            filteredSales.forEach(record => { currMonthSales[record.SaleDate] = 0; });  // For init of currMonthSales
+            filteredSales.forEach(record => {
+                console.log(record.SaleDate);
+                console.log(record.QuantitySold);
+                currMonthSales[record.SaleDate] += record.QuantitySold || 0;
+            });
+        }
+        console.log(currMonthSales);
+        return currMonthSales
     }
 };
