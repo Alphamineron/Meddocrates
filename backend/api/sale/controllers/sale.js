82ef154c52954490e84a72e11db931a92fba0dd0
Alphamineron <alphamineron@gmail.com>
2020-06-11 06:07:34 +0530

    Added Revenue of Each Day Fetch Route

--- backend/api/sale/controllers/sale.js
+++ backend/api/sale/controllers/sale.js
@@ -5,4 +5,28 @@
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+    async roed(ctx) {
+        const sales = await strapi.services.sale.find(ctx.query);
+
+        
+        let filteredSales = sales.filter(function (entry) {
+                                let givenDate = new Date(ctx.params["date"]);
+                                let SaleDate = new Date(entry.SaleDate);
+
+                                return SaleDate.getTime() === givenDate.getTime();
+                            });
+
+
+
+        if (Number(ctx.params["medUID"]) === -1) {
+            return filteredSales.map((fsale) => fsale.Revenue).reduce((a, b) => a + b, 0);
+        }
+        else {
+            filteredSales = filteredSales.filter(function (entry) {
+                return entry.MedUID.id === Number(ctx.params["medUID"])
+            });
+            return filteredSales.map((fsale) => fsale.Revenue).reduce((a, b) => a + b, 0);
+        }
+    }
+};
